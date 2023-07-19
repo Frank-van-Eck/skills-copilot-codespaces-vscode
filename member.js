82c9430fc171889357f2159b4f9cfcbad540e6a3
František Vaněk <83864686+Frank-van-Eck@users.noreply.github.com>
2023-07-19 20:47:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var memberList = document.getElementById("memberList");
+    var skillsList = document.getElementById("skillsList");
+    var projectsList = document.getElementById("projectsList");
+    var contactList = document.getElementById("contactList");
+    member.style.color = "#fff";
+    skills.style.color = "#000";
+    projects.style.color = "#000";
+    contact.style.color = "#000";
+    memberList.style.display = "block";
+    skillsList.style.display = "none";
+    projectsList.style.display = "none";
+    contactList.style.display = "none";
+}
